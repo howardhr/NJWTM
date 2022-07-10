1429984a92a7526fad7334ea71351fe6bea19ec6
Howard Hinestroza <froyrobledo99@gmail.com>
2022-07-09 20:48:00 -0500

    final

--- src/middlewares/verifySingup.js
+++ src/middlewares/verifySingup.js
@@ -5,7 +5,7 @@ export const checkDuplicateUsernameOrEmail = async (req, res, next) => {
     try {
         const user = await User.findOne({ username: req.body.username });
         if (user)
-            return res.status(400).json({ message: "The user already exists" });
+            return res.status(400).json({ message: "el usuario ya existe" });
         const email = await User.findOne({ email: req.body.email });
         if (email)
             return res.status(400).json({ message: "The email already exists" });
